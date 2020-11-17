9536e040305b39d1de892141af7e549fe9f9e983
Ashvini Thomas <ashvinithomas@gmail.com>
2020-11-17 18:52:33 -0500

    created cards to hold information and created Kelvin converting function

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,11 +10,27 @@ $("#searchBtn").on("click", function (event) {
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function (displayWeather) {
+    }).then(function (data) {
+        console.log(data);
 
-        //function to show weather forecast of searched city
+        $("#dashboard").empty();
+
+        //convert Kelvin to degrees F
+        var convertK = (((parseInt(data.main.temp)) - 273.15) * (5/9) + 32);
+
+        //created card to hold weather information
         var card = $("<div>").addClass("card");
-        var 
+        var cardBody =$("<div>").addClass("card-body");
+        var title = $("<h2>").addClass("card-title").text(data.name);
+        var temp = $("<p>").addClass("card-text").text("Temperature: " + convertK.toFixed(2) + "℉");
+        var humidity = $("<p>").addClass("card-text").text("Humidity: " + data.main.humidity + "%");
+        var windSpeed = $("<p>").addClass("card-text").text("Wind Speed: " + data.wind.speed + "MPH");
+
+        //append text to dashboard in html
+        cardBody.append(title, temp, humidity, windSpeed);
+        card.append(cardBody);
+        $("#dashboard").append(card);
+
 
 
     });
